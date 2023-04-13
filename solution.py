effc0ce620a026a815c5d89d8417044a7889a70d
Anastasia <33247868+AnastasiaValova@users.noreply.github.com>
2023-04-13 12:35:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ from scipy import stats
 
 chat_id = 225497605 # Ваш chat ID, не меняйте название переменной
 
-def solution2(x: np.array, y: np.array) -> bool:
+def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.07
     return (stats.anderson_ksamp([x, y]).significance_level < alpha)
